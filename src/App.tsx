a485929a176e2548832b5fb82d58821fe4df4ce2
all4x <estudar0101@gmail.com>
2022-07-01 12:25:10 -0300

    vai dar erro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Counter from "./components/count";
+import DogImage from "./components/reposity";
 
 function App() {
   return (
-    <div className="font-roboto text-3xl bg-galaxy h-screen">
+    <div className="font-roboto text-3xl bg-gray-900 h-screen">
       <div className="xl:flex justify-center xl:space-x-5 py-6">
         <h1 className="text-center py-6 text-white">Alex | Rodrigues</h1>
         <div className="grid items-center justify-center">
@@ -12,8 +13,9 @@ function App() {
           />
         </div>
       </div>
-      <div>
-        <Counter />
+      <div className="space-y-5">
+        <h1 className="text-center text-2xl font-normal text-gray-200">Gerador de Dog🐶</h1>
+        <DogImage />
       </div>
     </div>
   );
---
+++ src/components/reposity.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function DogImage() {
+  const [dogImage, setDogImage] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((Response) => Response.json())
+      .then((Object) => {
+        setDogImage(Object.message);
+
+        setLoading(false);
+      });
+  }, []);
+
+  return loading ? (
+    <div className="text-center font-thin text-3xl text-white">Loadging...</div>
+  ) : (
+    <>
+      <div className="flex justify-center p-3 border-blue-500 border-2">
+        <img src={dogImage}></img>
+      </div>
+    </>
+  );
+}
+
+export default DogImage();
