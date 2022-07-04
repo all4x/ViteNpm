004076c0af5b133835c822ef22f2fe02b5c28cc6
all4x <estudar0101@gmail.com>
2022-07-04 00:47:46 -0300

    contador de dogs

--- src/components/catgenerator.tsx
+++ src/components/catgenerator.tsx
@@ -4,6 +4,8 @@ export function CatImage() {
   const [catImage, setCatImage] = useState<string | any>([]);
   const [loading, setLoading] = useState(false);
   const [newCat, setNewCat] = useState(false);
+  const [generator, newGenerator] = useState<any | number>(0)
+
 
   useEffect(() => {
     setLoading(true);
@@ -22,12 +24,14 @@ export function CatImage() {
     <div className="text-center font-thin text-3xl text-white">Loadging...</div>
   ) : (
     <div>
-      <div className="flex justify-center p-3">
+      <div className="flex-col justify-center p-3">
         <img src={catImage} className="h-60 w-60 border-2 border-blue-500"></img>
+
+        <span className="text-center flex justify-center pt-2 text-white text-xl">Você gerou {generator}</span>
       </div>
       <div className="flex justify-center pb-10">
         <button
-          onClick={() => setNewCat(false)}
+          onClick={() => { setNewCat(false), newGenerator(generator + 1) }}
           className=" p-2 text-white border-2 border-blue-50 bg-blue-600 font-semibold"
         >
           GERAR OTO
--- src/components/doggenerator.tsx
+++ src/components/doggenerator.tsx
@@ -4,6 +4,7 @@ export function DogImage() {
   const [dogImage, setDogImage] = useState<string | any>([]);
   const [loading, setLoading] = useState(false);
   const [newDog, setNewDog] = useState(false);
+  const [generator, newGenerator] = useState<number | any>(0);
 
   useEffect(() => {
     setLoading(true);
@@ -21,12 +22,15 @@ export function DogImage() {
     <div className="text-center font-thin text-3xl text-white">Loadging...</div>
   ) : (
     <>
-      <div className="flex justify-center p-3">
+      <div className="flex-col justify-center p-3">
         <img src={dogImage} className="h-60 w-60 border-2 border-blue-500"></img>
+
+
+        <span className="text-center flex justify-center pt-2 text-white text-xl">Você gerou {generator}</span>
       </div>
       <div className="flex justify-center mb-10">
         <button
-          onClick={() => setNewDog(false)}
+          onClick={() => { setNewDog(false), newGenerator(generator + 1) }}
           className=" p-2 text-white bg-blue-600 border-2 border-blue-50 font-semibold"
         >
           GERAR OTO
